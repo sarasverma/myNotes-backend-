dae8d42e335801736403b2b2ad79e6039b060088
Saras <78587510+sarasverma@users.noreply.github.com>
2022-04-02 17:07:25 +0530

    added create and fetch note

--- models/Notes.js
+++ models/Notes.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
+// user is a foreign key
 const NotesSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   title: { type: String, required: true },
   description: { type: String, required: true },
   tags: { type: String, default: "general" },
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const { body, validationResult } = require("express-validator");
+const Note = require("../models/Notes");
 
-router.get("/", (req, res) => {
-  obj = { name: "notes", data: "test" };
-  res.json(obj);
+// ROUTER : 1   create new note
+router.post(
+  "/createnote",
+  fetchuser,
+  [
+    body("title", "Enter valid user title").exists(),
+    body("description", "Enter atleast 7 character for descrition").isLength({
+      min: 7,
+    }),
+  ],
+  async (req, res) => {
+    // valididating parameters
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { title, description, tags } = req.body;
+      const newNote = new Note({
+        user: req.user.id,
+        title: title,
+        description: description,
+        tags: tags,
+      });
+      const savedNote = await newNote.save();
+      res.json(savedNote);
+    } catch (error) {
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+// ROUTER : 2   fetch all notes of a user
+router.get("/fetchnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
 });
+
+// ROUTER : 3   update a note
+router.put("/updatenote:id", fetchuser, async (req, res) => {});
+
+// ROURER : 4   delete a note
+router.delete("/deletenote:id", fetchuser);
 
 module.exports = router;
